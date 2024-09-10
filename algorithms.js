90faa9fdc3737a7a93136de7cff8a56673454204
Tonnie <antonymuriithi505@gmail.com>
2024-09-10 23:42:46 +0300

    Addded the recursive Fibonacci sequence

--- algorithms.js
+++ algorithms.js
@@ -63,3 +63,14 @@ function isPowerOfTwo(n) {
     return true
 }
 console.log(isPowerOfTwo(5))
+
+//Recursive Fibonacci Sequence
+function recursiveFibonacci(n) {
+    if (n < 2) {
+        return n
+    }
+    return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+}
+console.log(recursiveFibonacci(0))
+console.log(recursiveFibonacci(1))
+console.log(recursiveFibonacci(6))
