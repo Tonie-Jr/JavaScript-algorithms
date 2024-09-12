3cb2cfddde7de7169bf1002e963ea16113b06129
Tonnie <antonymuriithi505@gmail.com>
2024-09-12 23:39:01 +0300

    Add the linear search funtion

--- algorithms.js
+++ algorithms.js
@@ -108,3 +108,16 @@ function binarySearch(arr, target) {
 console.log(binarySearch([-5, 2, 4, 6, 10], 10))
 console.log(binarySearch([-5, 2, 4, 6, 10], 6))
 console.log(binarySearch([-5, 2, 4, 6, 10], 20))
+
+// Linear search
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i
+        }
+    }
+    return -1
+}
+console.log(linearSearch([-5, 2, 10, 4, 6],10))
+console.log(linearSearch([-5, 2, 10, 4, 6],6))
+console.log(linearSearch([-5, 2, 10, 4, 6],20))
