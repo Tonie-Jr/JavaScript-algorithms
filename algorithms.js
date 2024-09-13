f7f36803f9fac08a4521b2a55ae575ba5d06fc5d
Tonnie <antonymuriithi505@gmail.com>
2024-09-13 23:49:05 +0300

    added the quick sort function

--- algorithms.js
+++ algorithms.js
@@ -124,7 +124,7 @@ console.log(linearSearch([-5, 2, 10, 4, 6],20))
 
 // Insertion sort 
 function InsertionSort(arr) {
-    for(let i = 1; i > arr.length; i++) {
+    for(let i = 1; i < arr.length; i++) {
         let numberToInsert = arr[i]
         let j = i - 1
         while(j >= 0 && arr[j] > numberToInsert) {
@@ -138,3 +138,23 @@ function InsertionSort(arr) {
 const arr = [8, 20, -2, 4, -6]
 InsertionSort(arr)
 console.log(arr)
+
+//Quicksort function
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        }else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+console.log(quickSort(arr))
