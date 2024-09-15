eba4d8144417a35826e607848c4c2e5b71de9318
Tonnie <antonymuriithi505@gmail.com>
2024-09-15 23:18:31 +0300

    The sum all numbers in a range

--- algorithms.js
+++ algorithms.js
@@ -198,3 +198,22 @@ console.log(climbingStaircase(2))
 console.log(climbingStaircase(3))
 console.log(climbingStaircase(4))
 console.log(climbingStaircase(5))
+
+//Sum array function
+function sumAll(arr) {
+    let min, max;
+    if (arr[0]) {
+        max = arr[0];
+        min = arr[1];
+    }else {
+        max = arr[1];
+        min = arr[0];
+    }
+    let sum = 0;
+    for (let i = min; i => max; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+console.logO(sumAll([1, 4]))
