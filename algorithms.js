f66d97d78cd630d9ef7c1083654fcc57f4b4a011
Tonnie <antonymuriithi505@gmail.com>
2024-09-28 22:40:32 +0300

    Add the pyramid algorithm

--- algorithms.js
+++ algorithms.js
@@ -303,3 +303,18 @@ function rot13(str) {
     return output;
 }
 console.log(rot13("SERR PBQR PNZC"));
+
+//
+function pyramid(){
+    const mid = Math.floor((2*n-1)/2)
+    for(let row=0; row<n; row++) {
+        let line = ""
+        for(let col; col<2n-1; col++) {
+            if( col >= mid - row && col <= mid + row) {
+                 line += '#'
+            }else{
+            line += ' '
+        }
+    }
+}
+}
